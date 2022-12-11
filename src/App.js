48e91e2c9bb6f81ecd472675514a495a5336fc33
ImedAbbes <imed.abbes@gmail.com>
2022-12-11 14:21:35 +0100

    state chekpoint show btn and timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import style from "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      Person : {
+        fullName:"Zaraki",
+        bio:"BLEACH character",
+        profession:"Captain of gotei 13 holding the title of KENPACHI",
+        imgSrc:"https://www.bleach-bravesouls.com/assets/img/character/appearance/08/character_safwy_kenpachi-2_01.png",
+      },
+      shows: false,
+      mountingTime:0,
+  }}
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({mountingTime:this.state.mountingTime +=1})
+    },1000)
+  }
+  change=()=>{
+    this.setState({shows:!this.state.shows});
+    this.setState({mountingTime:0})};
+  render() {
+    return (
+      <div className="Person">
+        {this.state.shows?
+        <>
+          <h1>{this.state.Person.fullName}</h1>
+          <p>{this.state.Person.bio}</p>
+          <p>{this.state.Person.profession}</p>
+          <img src={this.state.Person.imgSrc} alt="zaraki img online" style={{maxWidth:"200px"}}/>
+        </>:<h1>click the show button</h1>}
+        <br />
+        <button onClick={this.change} className="btn">Show</button>
+        <p>Timer : {this.state.mountingTime} </p>
+      </div>
+    );
+  }
 }
 
 export default App;
